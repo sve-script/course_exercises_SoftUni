bb1e1f0004649bef2841b553f5d8b09b40a15782
sve-script <svetlio.ppz@gmail.com>
2023-09-27 00:20:56 +0300

    test

--- 2. JS Files FUNDAMENTAL/1. JS Fundamental/2. Data Types and Variables/1.3 More Exercises - Data Types and Variables/07. Triangle Area.js
+++ 2. JS Files FUNDAMENTAL/1. JS Fundamental/2. Data Types and Variables/1.3 More Exercises - Data Types and Variables/07. Triangle Area.js
@@ -1,8 +1,8 @@
-function triangleArea (a,b,c){
+function triangleArea(a, b, c) {
 
-    let s = 0.5 * (a+b+c);
-    let area = Math.sqrt(s * ((s-a)*(s-b)*(s-c)))
+    let s = 0.5 * (a + b + c);
+    let area = Math.sqrt(s * ((s - a) * (s - b) * (s - c)))
     console.log(area);
 
 }
-triangleArea(2,3.5,4)
+triangleArea(2, 3.5, 4)
