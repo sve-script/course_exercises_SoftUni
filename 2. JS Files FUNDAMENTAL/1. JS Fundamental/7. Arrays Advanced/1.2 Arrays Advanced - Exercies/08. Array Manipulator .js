43e5b34f285ba19190d3870b1968571bc7de9c74
sve-script <svetlio.ppz@gmail.com>
2023-10-15 21:23:08 +0300

    gladiatorInventory solve

--- 2. JS Files FUNDAMENTAL/1. JS Fundamental/7. Arrays Advanced/1.2 Arrays Advanced - Exercies/08. Array Manipulator .js
+++ 2. JS Files FUNDAMENTAL/1. JS Fundamental/7. Arrays Advanced/1.2 Arrays Advanced - Exercies/08. Array Manipulator .js
@@ -1,5 +1,7 @@
 function arrayManipulator(array, commands) {
 
+    array = array.map(Number);
+
     let i = 0;
 
     while (commands[i] !== `print`) {
@@ -10,20 +12,20 @@ function arrayManipulator(array, commands) {
 
         switch (command) {
             case `add`:
-                add(array, arrayCommand[0], arrayCommand[1]);
+                add(array, Number(arrayCommand[0]), Number(arrayCommand[1]));
                 break;
 
             case `addMany`:
-                let indx = arrayCommand.shift();
-                addMany(array, Number(indx), arrayCommand)
+                let indx = Number(arrayCommand.shift());
+                addMany(array, Number(indx), arrayCommand.map(Number))
                 break;
 
             case `contains`:
-                console.log(contains(array, arrayCommand[0]));
+                console.log(contains(array, Number(arrayCommand[0])));
                 break;
 
             case `remove`:
-                remove(array, arrayCommand[0]);
+                remove(array, Number(arrayCommand[0]));
                 break;
 
             case `shift`:
@@ -91,61 +93,6 @@ function arrayManipulator(array, commands) {
     }
 }
 
-function arrManipulator(nums, commands) {
-    for (let i = 0; i < commands.length; i++) {
-        let currentValues = commands[i].split(" ");
-        let currentCommand = currentValues.shift();
-        let sumPairs = 0;
-        let pairsOfSumNums = [];
-
-        switch (currentCommand) {
-            case "add":
-                let indexToAdd = Number(currentValues[0]);
-                let elementToAdd = Number(currentValues[1]);
-                nums.splice(indexToAdd, 0, elementToAdd);
-                break;
-            case "addMany":
-                let indexToAddMany = Number(currentValues.shift());
-                let elementToAddMany = currentValues;
-                let currentElementToAddMany = 0;
-                for (let i = elementToAddMany.length - 1; i >= 0; i--) {
-                    currentElementToAddMany = Number(elementToAddMany[i]);
-                    nums.splice(indexToAddMany, 0, currentElementToAddMany);
-                }
-                break;
-            case "contains":
-                console.log(nums.indexOf(Number(currentValues[0])));
-                break;
-            case "remove":
-                let indexToRemove = Number(currentValues[0]);
-                nums.splice(indexToRemove, 1);
-                break;
-            case "shift":
-                for (let i = 0; i < currentValues[0]; i++) {
-                    nums.push(nums.shift());
-                }
-                break;
-            case "sumPairs":
-                if (nums.length % 2 !== 0) {
-                    nums.push(0);
-                }
-                for (let i = 0; i < nums.length; i += 2) {
-                    sumPairs = nums[i] + nums[i + 1];
-                    pairsOfSumNums.push(sumPairs);
-                }
-                nums = [...pairsOfSumNums];
-                break;
-            case "print":
-                console.log(`[ ${nums.join(", ")} ]`);
-                return;
-        }
-    }
-}
-
 
 arrayManipulator([1, 2, 3, 4, 5],
-    [`sumPairs`, 'shift 1', 'remove 0', 'add 10 1', 'addMany 1 5 3 5 4 7 7 7 1 1 2 5 6 1', 'remove 0', 'shift -1', `sumPairs`, 'contains 8', 'contains 5', 'contains 11', 'remove 6', 'addMany 1', 'shift 9', 'add -1 1', `sumPairs`, 'print'])
-
-
-arrManipulator([1, 2, 3, 4, 5],
     [`sumPairs`, 'shift 1', 'remove 0', 'add 10 1', 'addMany 1 5 3 5 4 7 7 7 1 1 2 5 6 1', 'remove 0', 'shift -1', `sumPairs`, 'contains 8', 'contains 5', 'contains 11', 'remove 6', 'addMany 1', 'shift 9', 'add -1 1', `sumPairs`, 'print'])
---
+++ 2. JS Files FUNDAMENTAL/1. JS Fundamental/7. Arrays Advanced/1.2 Arrays Advanced - Exercies/09. Gladiator Inventory .js
@@ -0,0 +1,69 @@
+function gladiatorInventory(array) {
+
+    let startingInventory = array.splice(0, 1).join(` `).split(` `);
+
+    while (array.length > 0) {
+
+        let fullCommand = array.shift();
+        let arrayCommand = fullCommand.split(` `)//цялата команда разделена в масив
+        let command = arrayCommand.shift(); // само текстовата команда,
+
+
+        switch (command) {
+            case `Buy`:
+                buy(startingInventory, arrayCommand[0])
+                break;
+            case `Trash`:
+                trash(startingInventory, arrayCommand[0])
+                break;
+            case `Repair`:
+                repair(startingInventory, arrayCommand[0])
+                break;
+            case `Upgrade`:
+                upgrade(startingInventory, arrayCommand[0])
+                break;
+        }
+    }
+
+
+    function buy(arr, item) {
+        let check = arr.includes(item);
+        if (!check) {
+            return arr.push(item)
+        }
+        return;
+    }
+
+    function trash(arr, item) {
+        let check = arr.indexOf(item)
+        if (check >= 0) {
+            return arr.splice(check, 1)
+        }
+        return;
+    }
+
+    function repair(arr, item) {
+        let check = arr.indexOf(item);
+        if (check >= 0) {
+            arr.splice(check, 1);
+            arr.push(item);
+        }
+        return arr
+    }
+
+    function upgrade(arr, item) {
+        let itemSplit = item.split(`-`);
+        let check = arr.indexOf(itemSplit[0]);
+        if (check >= 0) {
+            arr.splice(check + 1, 0, item.replace(`-`, `:`))
+        }
+        return arr
+    }
+    console.log(startingInventory.join(` `));
+}
+gladiatorInventory(['SWORD Shield Spear',
+    'Trash Bow',
+    'Repair Shield',
+    'Upgrade Helmet-V']
+
+)
