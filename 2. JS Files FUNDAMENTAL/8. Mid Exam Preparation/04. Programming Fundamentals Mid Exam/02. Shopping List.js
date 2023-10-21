765a6520eb821159047cf6a15409cf91a8ea8f80
sve-script <svetlio.ppz@gmail.com>
2023-10-21 19:33:54 +0300

    shoppingList form 04 Exam add

--- 2. JS Files FUNDAMENTAL/8. Mid Exam Preparation/04. Programming Fundamentals Mid Exam/02. Shopping List.js
+++ 2. JS Files FUNDAMENTAL/8. Mid Exam Preparation/04. Programming Fundamentals Mid Exam/02. Shopping List.js
@@ -1,7 +1,76 @@
-function shoppingList() {
+function shoppingList(array) {
+
+    let startingList = array.shift().split("!");
+
+    while (array.length > 0) {
+
+        let fullCommand = array.shift().split(" ");
+        let command = fullCommand[0];
+
+        switch (command) {
+            case "Urgent":
+                urgent(startingList, fullCommand[1])
+                break;
+            case "Unnecessary":
+                unnecessary(startingList, fullCommand[1])
+                break;
+            case "Correct":
+                correct(startingList, fullCommand[1], fullCommand[2])
+                break;
+            case "Rearrange":
+                rearrange(startingList, fullCommand[1])
+                break;
+
+        }
+    }
 
 
+    function urgent(arr, item) {
+        let check = arr.includes(item);
+        if (!check) {
+            return arr.unshift(item);
+        }
+        return;
+    }
+
+    function unnecessary(arr, item) {
+        let check = arr.indexOf(item);
+        if (check >= 0) {
+            return arr.splice(check, 1);
+        }
+        return;
+    }
+
+    function correct(arr, itemRemove, itemAdd) {
+        let check = arr.indexOf(itemRemove);
+        if (check >= 0) {
+            return arr.splice(check, 1, itemAdd)
+        }
+        return;
+    }
+
+    function rearrange(arr, item) {
+        let check = arr.indexOf(item);
+        if (check >= 0) {
+            let movedItem = arr.splice(check, 1);
+            arr.push(movedItem)
+        }
+
+    }
+
+
+    console.log(startingList.join(`, `));
 }
+
+shoppingList(["Milk!Pepper!Salt!Water!Banana",
+    "Urgent Salt",
+    "Unnecessary Grapes",
+    "Correct Pepper Onion",
+    "Rearrange Grapes",
+    "Correct Tomatoes Potatoes",
+    "Go Shopping!"])
+
+
 
 // It's the end of the week, and it is time for you to go shopping, so you need to create a shopping list first.
 // Input
