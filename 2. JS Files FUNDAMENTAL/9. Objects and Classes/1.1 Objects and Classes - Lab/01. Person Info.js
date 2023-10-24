c6f00d6323ca8ba6dfee14639c94775ca65bc0a1
sve-script <svetlio.ppz@gmail.com>
2023-10-25 01:06:27 +0300

    object exercise

---
+++ 2. JS Files FUNDAMENTAL/9. Objects and Classes/1.1 Objects and Classes - Lab/01. Person Info.js
@@ -0,0 +1,17 @@
+function personInfo(firstName, lastName, age) {
+
+    let person = {};
+
+    person.firstName = firstName;
+    person.lastName = lastName;
+    person.age = age;
+
+
+    let person1 = {
+        name: "sve...",
+        age: 32,
+        city: "Stara Zagora",
+    };
+    console.log(person1.city);
+}
+personInfo("Peter", "Pan", "20")
