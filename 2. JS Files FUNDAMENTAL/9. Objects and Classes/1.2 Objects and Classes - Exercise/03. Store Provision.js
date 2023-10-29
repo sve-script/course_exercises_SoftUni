9d50c6c41c2cae7a891f6ae3c8dd2b58a063236f
Svetoslav <145283458+sve-script@users.noreply.github.com>
2023-10-29 22:56:45 +0200

    store

--- 2. JS Files FUNDAMENTAL/9. Objects and Classes/1.2 Objects and Classes - Exercise/03. Store Provision.js
+++ 2. JS Files FUNDAMENTAL/9. Objects and Classes/1.2 Objects and Classes - Exercise/03. Store Provision.js
@@ -1,21 +1,30 @@
 function storeProvision(stock, orders) {
-
-    let stockAndOrders = stock.concat(orders);
-
     let products = {};
 
-    for (let i = 0, y = i + 1; i < stockAndOrders.length; i += 2, y += 2) {
-        let product = stockAndOrders[i];
-        let quantity = Number(stockAndOrders[y]);
+    for (let i = 0; i < stock.length; i += 2) {
+        let productName = stock[i];
+        let quantity = Number(stock[i + 1]);
 
-
-        products[product] = product;
-
+        products[productName] = quantity;
     }
-    console.log(products);
 
+    for (let j = 0; j < orders.length; j += 2) {
+        let orderProducts = orders[j];
+        let orderQuantity = Number(orders[j + 1]);
+
+
+        if (orderProducts in products) {
+            products[orderProducts] += orderQuantity;
+        } else {
+            products[orderProducts] = orderQuantity;
+        }
+    }
+
+    for (let key in products) {
+        console.log(`${key} -> ${products[key]}`)
+    }
 }
 storeProvision(
-    ['Chips', '5', 'CocaCola', '9', 'Bananas', '14', 'Pasta', '4', 'Beer', '2'],
-    ['Flour', '44', 'Oil', '12', 'Pasta', '7', 'Tomatoes', '70', 'Bananas', '30']
+    ["Chips", "5", "CocaCola", "9", "Bananas", "14", "Pasta", "4", "Beer", "2"],
+    ["Flour", "44", "Oil", "12", "Pasta", "7", "Tomatoes", "70", "Bananas", "30"]
 )
