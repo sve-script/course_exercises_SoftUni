9db224372d58be9060c88573b529ec9ae866d335
sve-script <svetlio.ppz@gmail.com>
2023-11-01 22:17:55 +0200

    songs commit

--- 2. JS Files FUNDAMENTAL/9. Objects and Classes/1.1 Objects and Classes - Lab/06. Songs.js
+++ 2. JS Files FUNDAMENTAL/9. Objects and Classes/1.1 Objects and Classes - Lab/06. Songs.js
@@ -1,12 +1,30 @@
 function songs(array) {
 
+    let songsNumber = array.shift();
 
+    class Songs {
+        constructor(typeList, name, time) {
+            this.typeList = typeList
+            this.name = name
+            this.time = time
+        }
 
+    }
 
+    let type = array.pop();
+
+    for (let songsInfo of array) {
+        let [typeList, name, time] = songsInfo.split("_");
+        let obj = new Songs(typeList, name, time);
+        if (obj.typeList == type || type == "all") {
+            console.log(obj.name);
+        }
+    }
 }
-songs([3,
-    'favourite_DownTown_3:14',
-    'favourite_Kiss_4:16',
-    'favourite_Smooth Criminal_4:01',
-    'favourite']
+songs([2,
+    'like_Replay_3:15',
+    'ban_Photoshop_3:48',
+    'all']
+
+
 )
