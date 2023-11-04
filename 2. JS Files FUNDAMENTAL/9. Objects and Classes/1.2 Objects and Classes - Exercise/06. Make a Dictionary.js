cc8e8cc0cd68299c1bd6944cca348aa377b80705
sve-script <svetlio.ppz@gmail.com>
2023-11-04 12:48:01 +0200

    make a dictionary  not finish yet

---
+++ 2. JS Files FUNDAMENTAL/9. Objects and Classes/1.2 Objects and Classes - Exercise/06. Make a Dictionary.js
@@ -0,0 +1,40 @@
+function makeADictionary(jsonArray) {
+
+    let objArray = [];
+
+    for (let json of jsonArray) {
+
+        let parseValue = JSON.parse(json);
+        let entries = Object.entries(parseValue)
+        objArray.push(parseValue)
+    }
+
+    let sortedArray = objArray.sort((a, b) => {
+        let keyOne = Object.keys(a)[0];
+        let keyTwo = Object.keys(b)[0];
+
+        if (keyOne < keyTwo) {
+            return -1;
+        } else if (keyOne > keyTwo) {
+            return 1;
+        } else {
+            return 0;
+        }
+    })
+
+
+
+    for (let obj of sortedArray) {
+        for (let key in obj) {
+            console.log(`Term: ${key} => Definition: ${obj[key]}`);
+        }
+    }
+
+}
+makeADictionary([
+    '{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
+    '{"Coffee":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
+    '{"Boiler":"A fuel-burning apparatus or container for heating water."}',
+    '{"Tape":"A narrow strip of material, typically used to hold or fasten something."}',
+    '{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}'
+])
