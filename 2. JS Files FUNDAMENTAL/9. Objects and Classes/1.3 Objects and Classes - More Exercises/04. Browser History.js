a9e8a151ffdb7afeba12b5b71036fd023544e867
sve... <145283458+sve-script@users.noreply.github.com>
2023-11-18 00:26:21 +0200

    80/100 solve Browser Histoyry

--- 2. JS Files FUNDAMENTAL/9. Objects and Classes/1.3 Objects and Classes - More Exercises/04. Browser History.js
+++ 2. JS Files FUNDAMENTAL/9. Objects and Classes/1.3 Objects and Classes - More Exercises/04. Browser History.js
@@ -3,21 +3,68 @@ function browserHistory(browserInfo, array) {
 
     for (let el of array) {
         let [command, site] = el.split(` `);
-        console.log(command);
 
+        if (command !== `Clear`) {
+            browserInfo[`Browser Logs`].push(el)
+        }
+
+        switch (command) {
+            case `Open`:
+                open(browserInfo, site)
+                continue;
+            case `Close`:
+                close(browserInfo, site)
+                break;
+
+            case `Clear`:
+                clear(browserInfo)
+                continue;
+
+        }
 
     }
 
+
+    for (let key in browserInfo) {
+        if (key == `Browser Name`) {
+            console.log(`${browserInfo[key]}`);
+        } else {
+            console.log(`${key}: ${browserInfo[key].join(`, `)}`);
+        }
+    }
+
+    function open(info, site) {
+
+        return info[`Open Tabs`].push(site)
+    }
+
+    function close(info, site) {
+
+        let openedSites = info[`Open Tabs`];
+        let indx = openedSites.indexOf(site);
+
+        if (indx !== -1) {
+            info[`Recently Closed`].push(site)
+            return openedSites.splice(indx, 1)
+        }
+    }
+
+    function clear(info) {
+
+        info[`Open Tabs`] = [];
+        info[`Recently Closed`] = [];
+        info[`Browser Logs`] = [];
+    }
 
 
 
 }
 browserHistory({
-    "Browser Name": "Mozilla Firefox",
-    "Open Tabs": ["YouTube"],
-    "Recently Closed": ["Gmail", "Dropbox"],
-    "Browser Logs": ["Open Gmail", "Close Gmail", "Open Dropbox", "Open YouTube", "Close Dropbox"]
+    "Browser Name": "Google Chrome",
+    "Open Tabs": ["asd", "a", "Googlbe Translate"],
+    "Recently Closed": ["Yahoo", "Gmail"],
+    "Browser Logs": ["Open YouTube", "Open Yahoo", "Open Google Translate", "Close Yahoo", "Open Gmail", "Close Gmail", "Close Gmail", "Open Facebook"]
 },
-    ["Open Wikipedia", "Clear History and Cache", "Open Twitter"]
+    ["Close Facebook", "Open StackOverFlow", "Open Google"]
 
 )
