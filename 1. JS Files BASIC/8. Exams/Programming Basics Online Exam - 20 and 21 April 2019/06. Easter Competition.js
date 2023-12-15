7d5bd0b366394258aac2a948c78f8652f84fd271
sve... <145283458+sve-script@users.noreply.github.com>
2023-12-15 22:30:14 +0200

    solve

--- 1. JS Files BASIC/8. Exams/Programming Basics Online Exam - 20 and 21 April 2019/06. Easter Competition.js
+++ 1. JS Files BASIC/8. Exams/Programming Basics Online Exam - 20 and 21 April 2019/06. Easter Competition.js
@@ -2,34 +2,69 @@ function easterCompetition(input) {
 
     let kozunakNumber = Number(input[0]);
 
-    for (let i = 0; i < input.length; i++) {
+    let chefName = "";
+    let totalScore = 0;
 
-        let command = input[i + 1];
-        let currentPoints = 0;
+    for (let i = 1; i < input.length; i++) {
+
+        let name = input[i];
+        i++;
+        let command = input[i];
+
+        let totalCurrentScore = 0;
 
         while (command !== "Stop") {
 
+            totalCurrentScore += Number(command);
 
-
+            i++;
+            command = input[i];
         }
 
+        if (totalScore < totalCurrentScore) {
+            chefName = name;
+            totalScore = totalCurrentScore;
+
+            console.log(`${chefName} has ${totalScore} points.`);
+            console.log(`${chefName} is the new number 1!`);
+            continue;
+        }
+
+        console.log(`${name} has ${totalCurrentScore} points.`);
     }
+
+    console.log(`${chefName} won competition with ${totalScore} points!`);
+
 }
-easterCompetition(["3",
-    "Chef Manchev",
-    "10",
-    "10",
-    "10",
-    "10",
-    "Stop",
-    "Natalie",
-    "8",
-    "2",
+easterCompetition(["2",
+    "Chef Angelov",
+    "9",
+    "9",
     "9",
     "Stop",
-    "George",
-    "9",
-    "2",
-    "4",
-    "2",
+    "Chef Rowe",
+    "10",
+    "10",
+    "10",
+    "10",
     "Stop"])
+
+
+// easterCompetition(["3",
+//     "Chef Manchev",
+//     "10",
+//     "10",
+//     "10",
+//     "10",
+//     "Stop",
+//     "Natalie",
+//     "8",
+//     "2",
+//     "9",
+//     "Stop",
+//     "George",
+//     "9",
+//     "2",
+//     "4",
+//     "2",
+//     "Stop"])
