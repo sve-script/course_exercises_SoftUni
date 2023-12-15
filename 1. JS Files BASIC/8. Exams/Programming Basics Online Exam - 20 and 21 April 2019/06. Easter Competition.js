38c93710de30ccb2500ea5b3857f4a29be6ed5b9
sve... <145283458+sve-script@users.noreply.github.com>
2023-12-15 22:02:01 +0200

    solve

---
+++ 1. JS Files BASIC/8. Exams/Programming Basics Online Exam - 20 and 21 April 2019/06. Easter Competition.js
@@ -0,0 +1,35 @@
+function easterCompetition(input) {
+
+    let kozunakNumber = Number(input[0]);
+
+    for (let i = 0; i < input.length; i++) {
+
+        let command = input[i + 1];
+        let currentPoints = 0;
+
+        while (command !== "Stop") {
+
+
+
+        }
+
+    }
+}
+easterCompetition(["3",
+    "Chef Manchev",
+    "10",
+    "10",
+    "10",
+    "10",
+    "Stop",
+    "Natalie",
+    "8",
+    "2",
+    "9",
+    "Stop",
+    "George",
+    "9",
+    "2",
+    "4",
+    "2",
+    "Stop"])
