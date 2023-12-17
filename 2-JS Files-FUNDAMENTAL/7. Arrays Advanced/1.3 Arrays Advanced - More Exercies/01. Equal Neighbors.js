e187a7c6d156d8f4adda574b12dfcca698e806d9
sve... <145283458+sve-script@users.noreply.github.com>
2023-12-17 12:06:38 +0200

    solve

--- 2-JS Files-FUNDAMENTAL/7. Arrays Advanced/1.3 Arrays Advanced - More Exercies/01. Equal Neighbors.js
+++ 2-JS Files-FUNDAMENTAL/7. Arrays Advanced/1.3 Arrays Advanced - More Exercies/01. Equal Neighbors.js
@@ -2,25 +2,29 @@ function equalNeighbors(matrix) {
 
     let neighbors = 0;
 
-    for (let row = 0; row < matrix.length; row++) {
-        for (let col = 0; col < matrix[row].length; col++) {
-            if (row < matrix.length - 1) {
-                if (matrix[row][col] == matrix[row + 1][col]) {
-                    neighbors++;
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+
+            if (j < matrix[i].length - 1) {
+                if (matrix[i][j] == matrix[i][j + 1]) {
+                    neighbors++
                 }
             }
-            if (col < matrix[row].length) {
-                if (matrix[row][col] == matrix[row][col + 1]) {
-                    neighbors++;
+            if (i < matrix.length - 1) {
+                if (matrix[i][j] == matrix[i + 1][j]) {
+                    neighbors++
                 }
             }
         }
     }
+
     return neighbors
 }
 
 equalNeighbors([['test', 'yo', 'yo', 'ho'],
 ['well', 'done', 'no', '6'],
 ['not', 'done', 'yet', '5']])
+
+
 
 
