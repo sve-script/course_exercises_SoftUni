26cead9e6458e6dae468d807cca5b75cd51a0440
sve... <145283458+sve-script@users.noreply.github.com>
2023-12-20 16:06:02 +0200

    s

--- 3.1-JS files - Advanced/1. Syntax, Functions and Statements/1.1 Syntax, Functions and Statements - Lab/09. Square of Stars.js
+++ 3.1-JS files - Advanced/1. Syntax, Functions and Statements/1.1 Syntax, Functions and Statements - Lab/09. Square of Stars.js
@@ -1,7 +1,13 @@
-function squareOfStars() {
+function squareOfStars(params = 5) {
 
+    for (let i = 0; i < params; i++) {
 
+        let printValue = "";
 
-
+        for (let j = 0; j < params; j++) {
+            printValue += "* ";
+        }
+        console.log(printValue);
+    }
 }
 squareOfStars()
