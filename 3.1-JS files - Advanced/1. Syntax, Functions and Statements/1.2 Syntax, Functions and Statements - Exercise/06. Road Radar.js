ab9c53169b0da40315767c802836268648d1fe15
sve... <145283458+sve-script@users.noreply.github.com>
2023-12-22 00:47:13 +0200

    s

--- 3.1-JS files - Advanced/1. Syntax, Functions and Statements/1.2 Syntax, Functions and Statements - Exercise/06. Road Radar.js
+++ 3.1-JS files - Advanced/1. Syntax, Functions and Statements/1.2 Syntax, Functions and Statements - Exercise/06. Road Radar.js
@@ -1,4 +1,48 @@
-function roadRider(params) {
+function roadRider(kmH, area) {
+
+    let speedLimit = 0;
+
+    let status;
+    let difference = 0;
+
+    let flag = false;
+
+    switch (area) {
+        case "motorway":
+            speedLimit = 130
+            difference = kmH - speedLimit;
+            break;
+        case "interstate":
+            speedLimit = 90
+            difference = kmH - speedLimit;
+            break;
+        case "city":
+            speedLimit = 50;
+            difference = kmH - speedLimit;
+            break;
+        case "residential":
+            speedLimit = 20;
+            difference = kmH - speedLimit;
+            break;
+    }
+
+    if (difference <= 0) {
+        console.log(`Driving ${kmH} km/h in a ${speedLimit} zone`);
+    } else {
+        flag = true;
+    }
+
+    if (flag) {
+        if (difference <= 20) {
+            status = `speeding`;
+        } else if (difference <= 40) {
+            status = `excessive speeding`
+        } else {
+            status = `reckless driving`
+        }
+
+        console.log(`The speed is ${Math.abs(difference)} km/h faster than the allowed speed of ${speedLimit} - ${status}`);
+    }
 
 }
-roadRider()
+roadRider(130, 'motorway')
