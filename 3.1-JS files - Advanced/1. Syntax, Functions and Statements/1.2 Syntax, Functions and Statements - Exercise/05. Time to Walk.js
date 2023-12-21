46d652f06ff63b417b54ff0fc44f1111fe0c0acc
sve... <145283458+sve-script@users.noreply.github.com>
2023-12-21 23:48:09 +0200

    s

--- 3.1-JS files - Advanced/1. Syntax, Functions and Statements/1.2 Syntax, Functions and Statements - Exercise/05. Time to Walk.js
+++ 3.1-JS files - Advanced/1. Syntax, Functions and Statements/1.2 Syntax, Functions and Statements - Exercise/05. Time to Walk.js
@@ -1,7 +1,44 @@
-function timeToWalk(params) {
+function timeToWalk(steps, length, speed) {
 
+    let university = steps * length;
 
+    let breaksMinutes = Math.floor(university / 1000 / 0.5)
 
+    let secondsInMetres = (speed * 1000) / 3600;
 
+    let totalTimeInSeconds = Math.ceil(university / secondsInMetres);
+
+    let s = 0;
+    let m = 0;
+    let h = 0;
+
+    for (let i = 1; i <= totalTimeInSeconds; i++) {
+        s++;
+
+        if (s > 59) {
+            m++
+            s = 0
+        }
+        if (m > 59) {
+            h++
+            s = 0;
+            m = 0;
+        }
+    }
+
+    m += breaksMinutes;
+
+    if (h < 10) {
+        h = `0${h}`;
+    }
+    if (m < 10) {
+        m = `0${m}`
+    }
+    if (s < 10) {
+        s = `0${s}`;
+    }
+
+    console.log(`${h}:${m}:${s}`);
 
 }
+timeToWalk(4000, 1, 4)
