533538da6b64762c557499cb0b3340954ed82a53
sve... <145283458+sve-script@users.noreply.github.com>
2023-12-23 00:21:27 +0200

    s

--- 3. JS Web Developer/1. JS Advanced/3.1-JS files - Advanced/2. Arrays and Nested Arrays/2.1 Arrays and Nested Arrays - Lab/08. Process Odd Positions.js
+++ 3. JS Web Developer/1. JS Advanced/3.1-JS files - Advanced/2. Arrays and Nested Arrays/2.1 Arrays and Nested Arrays - Lab/08. Process Odd Positions.js
@@ -1,12 +1,20 @@
+// function proccessOddPositions(params) {
+
+//     let newArr = [];
+
+//     for (let i = 1; i < params.length; i += 2) {
+//         newArr.push(params[i] * 2);
+//     }
+
+//     return newArr.reverse().join(` `)
+
+// }
+// console.log(proccessOddPositions([3, 0, 10, 4, 7, 3]));
+
+
 function proccessOddPositions(params) {
 
-    let newArr = [];
-
-    for (let i = 1; i < params.length; i += 2) {
-        newArr.push(params[i] * 2);
-    }
-
-    return newArr.reverse().join(` `)
-
+    const result = params.filter((x, i) => i % 2 == 1).map(x => x * 2).reverse().join(` `);
+    return result;
 }
-console.log(proccessOddPositions([3, 0, 10, 4, 7, 3]));
+proccessOddPositions([3, 0, 10, 4, 7, 3]);
---
+++ 3. JS Web Developer/1. JS Advanced/3.1-JS files - Advanced/2. Arrays and Nested Arrays/2.1 Arrays and Nested Arrays - Lab/11. Equal Neighbors.js
@@ -0,0 +1,3 @@
+function EqualNeighbors(params) {
+
+}
