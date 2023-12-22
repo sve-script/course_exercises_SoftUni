1be55dee8ba2570c043247b52d34070d26abb059
sve... <145283458+sve-script@users.noreply.github.com>
2023-12-22 20:57:32 +0200

    s

--- 3.1-JS files - Advanced/1. Syntax, Functions and Statements/1.2 Syntax, Functions and Statements - Exercise/08. Validity Checker.js
+++ 3.1-JS files - Advanced/1. Syntax, Functions and Statements/1.2 Syntax, Functions and Statements - Exercise/08. Validity Checker.js
@@ -1,27 +1,31 @@
 function validityChecker(x1, x2, y1, y2) {
 
-    function distance(x1, y1, x2, y2) {
-        let distH = x1 - x2;
-        let distY = y1 - y2;
-        return Math.sqrt(distH ** 2 + distY ** 2);
+    function calculateDistance(x1, y1, x2, y2) {
+        console.log(Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)));
+        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+
     }
 
-    if (Number.isInteger(distance(x1, y1, 0, 0))) {
+    const distance1ToOrigin = calculateDistance(x1, y1, 0, 0);
+    const distance2ToOrigin = calculateDistance(x2, y2, 0, 0);
+    const distance1To2 = calculateDistance(x1, y1, x2, y2);
+
+    if (Number.isInteger(distance1ToOrigin)) {
         console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
     } else {
         console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
     }
 
-    if (Number.isInteger(distance(x2, y2, 0, 0))) {
+    if (Number.isInteger(distance2ToOrigin)) {
         console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
     } else {
         console.log(`{${x2}, ${y2}} to {0, 0} is invalid`);
     }
 
-    if (Number.isInteger(distance(x1, y1, x2, y2))) {
+    if (Number.isInteger(distance1To2)) {
         console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
     } else {
         console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
     }
 }
-validityChecker(3.3, 0, 0, 4)
+validityChecker(3, 0, 0, 1)
