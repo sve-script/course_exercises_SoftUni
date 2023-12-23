dca44d216e8be81ac199985a2a94d617c626d13d
sve... <145283458+sve-script@users.noreply.github.com>
2023-12-23 20:59:00 +0200

    s

--- 3. JS Web Developer/1. JS Advanced/3.1-JS files - Advanced/2. Arrays and Nested Arrays/2.1 Arrays and Nested Arrays - Lab/11. Equal Neighbors.js
+++ 3. JS Web Developer/1. JS Advanced/3.1-JS files - Advanced/2. Arrays and Nested Arrays/2.1 Arrays and Nested Arrays - Lab/11. Equal Neighbors.js
@@ -1,3 +1,35 @@
 function EqualNeighbors(params) {
 
+
+    let pairs = 0;
+
+    for (let i = 0; i < params.length; i++) {
+
+        for (let j = 0; j < params[i].length; j++) {
+
+            if (j < params[i].length - 1) {
+
+                if (params[i][j] == params[i][j + 1]) {
+                    pairs++
+                }
+            }
+
+            if (i < params.length - 1) {
+
+                if (params[i][j] == params[i + 1][j]) {
+                    pairs++
+                }
+            }
+
+        }
+    }
+
+    return pairs;
 }
+
+EqualNeighbors([['test', 'yes', 'yo', 'ho'],
+['well', 'done', 'yo', '6'],
+['not', 'done', 'yet', '5']]
+
+
+)
